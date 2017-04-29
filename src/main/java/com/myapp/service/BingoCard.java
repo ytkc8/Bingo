e750e83e60583c3d2833c08fad9652ad74fbd0da
yan_toshimichi_kawachi <k.toshimichi@gmail.com>
2017-04-30 00:47:21 +0900

    Adds isBingo method to BingoBoard

--- src/main/java/com/myapp/service/BingoCard.java
+++ src/main/java/com/myapp/service/BingoCard.java
@@ -15,14 +15,14 @@ public class BingoCard {
     public BingoCard() {
         Random random = new Random();
         List<Integer> settedNum = new ArrayList<>();
-        cells = new ArrayList<>(pieceCellNum * pieceCellNum);
-        for (int i = 0; i < pieceCellNum; i ++) {
+        this.cells = new ArrayList<>(pieceCellNum * pieceCellNum);
+        for (int i = 0; i < pieceCellNum; i++) {
             for (int j = 0; j < pieceCellNum; j++) {
                 while (true) {
                     int fieldNum = random.nextInt(ballNum);
                     if (!settedNum.contains(fieldNum)) {
                         settedNum.add(fieldNum);
-                        cells.add(new Cell(fieldNum + 1, j, i));
+                        this.cells.add(new Cell(fieldNum + 1, j, i));
                         break;
                     }
                 }
@@ -35,8 +35,8 @@ public class BingoCard {
     }
 
     boolean isExist(int vertical, int horizontal) {
-        if (vertical >= 0 && pieceCellNum > vertical) {
-            if (horizontal >= 0 && pieceCellNum > horizontal) {
+        if (0 <= vertical && vertical < pieceCellNum) {
+            if (0 <= horizontal && horizontal < pieceCellNum) {
                 return true;
             }
         }
@@ -50,11 +50,82 @@ public class BingoCard {
         return Optional.empty();
     }
 
+    Optional<Cell> findCell(int fieldNum) {
+        return this.cells.stream()
+                .filter(cell -> cell.getFieldNum() == fieldNum)
+                .findFirst();
+    }
+
+    public void openCell(int fieldNum) {
+        findCell(fieldNum).ifPresent(cell -> {
+            cell.setWasOpened(true);
+        });
+    }
+
+    public boolean isBingo() {
+        for (int i = 0; i < pieceCellNum; i++) {
+            if (isVerticalBingo(i)) return true;
+            if (isHorizontalBingo(i)) return true;
+        }
+        if (isSlantingBingo()) return true;
+        return false;
+    }
+
+    private boolean isVerticalBingo(int vertical) {
+        List<Cell> openedCells = new ArrayList<>();
+        for (int i = 0; i < pieceCellNum; i++) {
+            getCell(vertical, i).filter(Cell::isWasOpened).ifPresent(cell -> {
+                openedCells.add(cell);
+            });
+        }
+        if (openedCells.size() >= 5) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isHorizontalBingo(int horizontal) {
+        List<Cell> openedCells = new ArrayList<>();
+        for (int i = 0; i < pieceCellNum; i++) {
+            getCell(i, horizontal).filter(Cell::isWasOpened).ifPresent(cell -> {
+                openedCells.add(cell);
+            });
+        }
+        if (openedCells.size() >= 5) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isSlantingBingo() {
+        if (getCell(0, 0).filter(Cell::isWasOpened).isPresent() &&
+                getCell(1, 1).filter(Cell::isWasOpened).isPresent() &&
+                getCell(2, 2).filter(Cell::isWasOpened).isPresent() &&
+                getCell(3, 3).filter(Cell::isWasOpened).isPresent() &&
+                getCell(4, 4).filter(Cell::isWasOpened).isPresent()
+                ) {
+            return true;
+        } else if (getCell(4, 0).filter(Cell::isWasOpened).isPresent() &&
+                getCell(3, 1).filter(Cell::isWasOpened).isPresent() &&
+                getCell(2, 2).filter(Cell::isWasOpened).isPresent() &&
+                getCell(1, 3).filter(Cell::isWasOpened).isPresent() &&
+                getCell(0, 4).filter(Cell::isWasOpened).isPresent()
+                ) {
+            return true;
+        }
+        return false;
+    }
+
     public void printBoardContent() {
         for (int i = 0; i < pieceCellNum; i++) {
             for (int j = 0; j < pieceCellNum; j++) {
                 getCell(j, i).ifPresent(cell -> {
-                    System.out.print("[" + cell.getFieldNum() + "]\t");
+                    int fieldNum = cell.getFieldNum();
+                    if (fieldNum < 10) {
+                        System.out.print("[ " + fieldNum + "]\t");
+                    } else {
+                        System.out.print("[" + fieldNum + "]\t");
+                    }
                 });
             }
             System.out.println();
--- src/test/java/com/myapp/service/BingoCardTest.java
+++ src/test/java/com/myapp/service/BingoCardTest.java
@@ -12,6 +12,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+@SuppressWarnings("OptionalGetWithoutIsPresent")
 public class BingoCardTest {
     private BingoCard bingoCard;
 
@@ -64,5 +65,76 @@ public class BingoCardTest {
 
         assertThat(maybeCell1.isPresent(), is(false));
         assertThat(maybeCell2.isPresent(), is(false));
+    }
+
+    @Test
+    public void test_findCell_returnsCell() throws Exception {
+        bingoCard.getCells().forEach(cell -> cell.setFieldNum(0));
+        bingoCard.getCell(1, 2).get().setFieldNum(10);
+
+        Cell cell = bingoCard.findCell(10).get();
+
+        assertThat(cell.getVertical(), equalTo(1));
+        assertThat(cell.getHorizontal(), equalTo(2));
+        assertThat(cell.getFieldNum(), equalTo(10));
+    }
+
+    @Test
+    public void test_findCell_returnsEmpty() throws Exception {
+        bingoCard.getCells().forEach(cell -> cell.setFieldNum(0));
+
+        Optional<Cell> cell = bingoCard.findCell(10);
+
+        assertThat(cell.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void test_openCell_onSuccess() throws Exception {
+        bingoCard.getCells().forEach(cell -> cell.setFieldNum(0));
+        bingoCard.getCell(1, 2).get().setFieldNum(10);
+        assertThat(bingoCard.getCell(1, 2).get().isWasOpened(), equalTo(false));
+
+        bingoCard.openCell(10);
+
+        assertThat(bingoCard.getCell(1, 2).get().isWasOpened(), equalTo(true));
+    }
+
+    @Test
+    public void test_openCell_onFailure() throws Exception {
+        bingoCard.getCells().forEach(cell -> cell.setFieldNum(0));
+
+        bingoCard.openCell(11);
+
+        assertThat(bingoCard.getCells().stream()
+                .anyMatch(Cell::isWasOpened), equalTo(false));
+    }
+
+    @Test
+    public void test_isBingo_returnsTrue_whenVerticalLineBingo() throws Exception {
+        bingoCard.getCells().stream()
+                .filter(cell -> cell.getVertical() == 2)
+                .forEach(cell -> cell.setWasOpened(true));
+
+        assertThat(bingoCard.isBingo(), equalTo(true));
+    }
+
+    @Test
+    public void test_isBingo_returnsTrue_whenHorizontalLineBingo() throws Exception {
+        bingoCard.getCells().stream()
+                .filter(cell -> cell.getHorizontal() == 0)
+                .forEach(cell -> cell.setWasOpened(true));
+
+        assertThat(bingoCard.isBingo(), equalTo(true));
+    }
+
+    @Test
+    public void test_isBingo_returnsTrue_whenSlantingBingo() throws Exception {
+        bingoCard.getCell(0, 0).get().setWasOpened(true);
+        bingoCard.getCell(1, 1).get().setWasOpened(true);
+        bingoCard.getCell(2, 2).get().setWasOpened(true);
+        bingoCard.getCell(3, 3).get().setWasOpened(true);
+        bingoCard.getCell(4, 4).get().setWasOpened(true);
+
+        assertThat(bingoCard.isBingo(), equalTo(true));
     }
 }
